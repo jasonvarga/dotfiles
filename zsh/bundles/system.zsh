915b93e4b3555c5ae15365c21abe8c42afc63ffc
Jason Varga <jason@pixelfear.com>
2021-09-28 10:48:44 -0400

    most used commands

--- zsh/bundles/system.zsh
+++ zsh/bundles/system.zsh
@@ -2,3 +2,7 @@ alias flushdns='dscacheutil -flushcache'
 alias ip='curl icanhazip.com'
 alias ip4='curl ipv4.icanhazip.com'
 alias ip6='curl ipv6.icanhazip.com'
+
+mostusedcommands() {
+    history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head -15
+}
