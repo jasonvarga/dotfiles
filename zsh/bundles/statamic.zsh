99f6ca41b63b0cfe086c3db140b7084bf23fda29
Jason Varga <jason@pixelfear.com>
2021-09-27 23:33:09 -0400

    statamic

--- zsh/bundles/statamic.zsh
+++ zsh/bundles/statamic.zsh
@@ -8,6 +8,8 @@ alias cmspath='comp show statamic/cms --path'
 alias s1='cd ~/code/statamic/one'
 alias s2='cd ~/code/statamic/two'
 alias s3='cd ~/code/statamic/three'
+alias plzuser="cp $DOTFILES/statamic/jason@statamic.com.yaml users/jason@statamic.com.yaml && echo 'User created.'"
+alias plzpro="sed -i \"\" \"s/'pro'\ =>\ false/'pro'\ =>\ true/\" config/statamic/editions.php && echo 'Pro enabled.'"
 
 function linkcp {
     rm -f public/vendor/statamic/cp
