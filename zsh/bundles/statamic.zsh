ab138104eae2987a9e6177120aae739558e311b9
Jason Varga <jason@pixelfear.com>
2021-10-03 17:36:50 -0400

    crcms will require the currently checked out branch

--- zsh/bundles/statamic.zsh
+++ zsh/bundles/statamic.zsh
@@ -3,7 +3,6 @@ alias plz='please'
 alias asc='plz stache:clear'
 alias pegs='p --exclude-group slow'
 alias cucms='comp update statamic/cms'
-alias crcms='comp require statamic/cms'
 alias cmspath='comp show statamic/cms --path'
 alias s1='cd ~/code/statamic/one'
 alias s2='cd ~/code/statamic/two'
@@ -14,4 +13,16 @@ alias plzpro="sed -i \"\" \"s/'pro'\ =>\ false/'pro'\ =>\ true/\" config/statami
 function linkcp {
     rm -f public/vendor/statamic/cp
     ln -s /Users/jason/Code/statamic/three/cms/resources/dist public/vendor/statamic/cp
+}
+
+function crcms() {
+    branch=$(cd ~/code/statamic/three/cms && git rev-parse --abbrev-ref HEAD)
+    if [[ $branch =~ ^[0-9]+\.[0-9]+$ ]]; then
+        constraint="$branch.x-dev"
+    else
+        constraint="dev-$branch"
+    fi
+    command="composer require \"statamic/cms $constraint\""
+    echo $command
+    eval $command
 }
