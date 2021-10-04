e4674993d99f7452e46ea1e1657c00f4cc83d629
Jason Varga <jason@pixelfear.com>
2021-10-04 12:31:03 -0400

    modals can have text while they're enabled

--- hammerspoon/modals.lua
+++ hammerspoon/modals.lua
@@ -4,12 +4,15 @@ function modal(name, key, opts)
   local mods = opts.mods or {}
   local timeout = opts.timeout or false
   local timer
+  local text = opts.text or false
+  local alert = nil
 
   hyper:bind(mods, key, function() modal:enter() end)
   modal:bind('', 'escape', nil, function() modal:exit() end)
 
   function modal:entered()
     log.d('modal ' .. name .. ' entered')
+    if text then alert = hs.alert(text, {}, nil, 'forever') end
     if timeout then
       timer = hs.timer.doAfter(timeout, function() modal:exit() end)
     end
@@ -18,6 +21,7 @@ function modal(name, key, opts)
   function modal:exited()
     log.d('modal ' .. name .. ' exited')
     if timer then timer:stop() end
+    if alert then hs.alert.closeSpecific(alert) end
   end
 
   return modal
--- hammerspoon/summon.lua
+++ hammerspoon/summon.lua
@@ -1,12 +1,13 @@
 local summon = {}
 
 summon.via = function(modalKey)
-  local modal = modal('Summon', modalKey, {timeout=1})
+  local modal = modal('Summon', modalKey, {timeout=1, text='Summoning...'})
 
   hs.fnutils.map(apps, function(app)
     if app.key then
       modal:bind(modalKey, app.key, function()
         hs.application.launchOrFocusByBundleID(app.id)
+        modal:exit()
       end)
     end
   end)
