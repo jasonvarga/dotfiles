1d1e444238633101cb9299332e6d8aa4ebc563e6
Jason Varga <jason@pixelfear.com>
2021-10-03 23:46:25 -0400

    move positions to its own file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,6 +5,7 @@
 require('helpers')
 require('modals')
 require('windows')
+positions = require('positions')
 apps = require('apps')
 hyper = require('hyper'):setHyperKey('F19')
 
@@ -28,47 +29,10 @@ require('summon').via('o')
 hs.grid.setGrid('30x20')
 hs.grid.setMargins('15x15') -- id like 30x30, but https://github.com/Hammerspoon/hammerspoon/issues/2955
 
-positions = {
-  full     = '0,0 30x20',
-
-  center = {
-    large  = '4,1 22x18',
-    medium = '6,1 18x18',
-    small  = '8,2 14x16',
-    tiny   = '9,3 12x12',
-    mini   = '11,4 8x10',
-  },
-
-  sixths = {
-    left  = '0,0 5x20',
-    right  = '25,0 5x20',
-  },
-
-  thirds = {
-    left   = '0,0 10x20',
-    center = '10,0 10x20',
-    right  = '20,0 10x20',
-  },
-
-  halves = {
-    left   = '0,0 15x20',
-    right  = '15,0 15x20',
-  },
-
-  twoThirds = {
-    left   = '0,0 20x20',
-    right  = '10,0 20x20',
-  },
-
-  fiveSixths = {
-    left   = '0,0 25x20',
-    right  = '5,0 25x20',
-  },
-}
-
 --------------------------------------------------------------------------------
 -- Grid Movements
 --------------------------------------------------------------------------------
+
 
 local chain = require('chain')
 local chainX = { 'thirds', 'halves', 'twoThirds', 'fiveSixths', 'sixths', }
