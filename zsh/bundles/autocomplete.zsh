9dbfcbd3b624caa006f51295204fd85334d73fb3
Jason Varga <jason@pixelfear.com>
2021-10-10 17:33:49 -0400

    dont use fzf

--- zsh/bundles/autocomplete.zsh
+++ zsh/bundles/autocomplete.zsh
@@ -3,12 +3,3 @@ zstyle ':autocomplete:*' fzf-completion yes
 zstyle ':autocomplete:*' list-lines 5
 zstyle ':autocomplete:*' min-delay 0.2
 zstyle ':autocomplete:*' min-input 1
-
-zle -N up-line-or-search
-up-line-or-search() {
-  if [[ $LBUFFER == *$'\n'* ]] then
-    zle up-line
-  else
-    fzf-history-widget
-  fi
-}
