b4bfaada113734aa1c64b0c730385282bcba8e5b
Jason Varga <jason@pixelfear.com>
2022-02-25 23:41:30 -0500

    git wip

--- zsh/bundles/git.zsh
+++ zsh/bundles/git.zsh
@@ -1,8 +1,8 @@
-alias g='git'
 alias gs='git status -s | grep -q . && echo "$(git status -s)" || echo "Clean as a whistle"'
 alias gss='git status'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gc='git commit -m'
+alias gco='git checkout'
 alias gcod='gco $(gdb)'
 alias gap="git add -p"
 alias gpl='git pull'
@@ -11,28 +11,36 @@ alias gpsh='git push'
 alias gnb='git checkout -b'
 alias nah='git reset --hard; git clean -df;'
 alias glt='git describe --tags --abbrev=0' # git latest tag
-alias gcslt='git log $(glt)..HEAD --oneline --no-decorate' # git commits since latest tag
+alias gcslt='git log $(glt)..HEAD --oneline --no-decorate --first-parent --no-merges' # git commits since latest tag
 alias gin="git init && gaa && gcmsg 'Initial commit.'"
 alias gpub='git push --set-upstream origin HEAD'
 alias gt='gittower .'
 alias gdb='git remote show origin | grep "HEAD branch" | cut -d " " -f5'
-alias gpr='gh pr checkout'
+alias gpro='gh pr view --web'
 alias wip="git add . && git commit -m 'wip'"
 alias wipa="git add . && git commit --amend -m 'wip'"
 alias cdr='cd $(git rev-parse --show-toplevel)'
 alias bisect='git bisect'
 
+# Git checkout PR
+gpr() {
+  if [ -n "$1" ]; then gh pr checkout $1; return; fi
+  pr=$(gh pr list --limit=100 | fzf +m | awk '{print $1}')
+  echo "Checking out PR #$pr..."
+  gh pr checkout $(echo "$pr")
+}
+
 # Git checkout with fzf
-gco() {
+gcob() {
   if [ -n "$1" ]; then git checkout $1; return; fi
-  local branches branch tags
+  local branches branch
   branches=$(git branch -vv)
   branch=$(echo "$branches" | fzf +m)
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
 
 # Git checkout tag with fzf
-gct() {
+gcot() {
   if [ -n "$1" ]; then git checkout $1; return; fi
   local tag tags
   tags=$(git tag)
@@ -63,4 +71,33 @@ function gdbard {
     git checkout $default_branch
     git branch -D $branch
     git pull
+}
+
+# Switches to the master branch and deletes the branch it was just on.
+# gdbarm = "Git Delete Branch And Return to Master"
+function gdbarm {
+    branch=$(git branch --show-current)
+    git checkout master
+    git branch -D $branch
+    git pull
+}
+
+# Check if a branch exists on the remote
+# grbe = "Git Remote Branch Exists"
+function grbe {
+    if [ -n "$1" ]; then
+        branch=$1
+    else
+        branch=$(git branch --show-current)
+    fi
+    exists=$(git ls-remote --exit-code --heads origin $branch)
+    code=$?
+    if [[ -n $exists ]]; then echo "Exists"; else echo "Does not exist"; fi
+    return $code
+}
+
+function gcolt() {
+    tag=$(glt)
+    echo "Checking out $tag"
+    gco $tag
 }
