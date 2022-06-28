649b182267e9d9f92bb10e20a2436e4270bc405b
Jason Varga <jason@pixelfear.com>
2022-06-28 09:49:35 -0400

    sail

--- zsh/bundles/laravel.zsh
+++ zsh/bundles/laravel.zsh
@@ -7,6 +7,7 @@ alias arti='comp install && arte && art key:generate && b'
 alias mfs='art migrate:fresh --seed'
 alias queue='art queue:listen'
 alias tink='tinker'
+alias sail='vendor/bin/sail'
 
 function tinker {
 	if [ -e artisan ]; then
@@ -16,4 +17,14 @@ function tinker {
 	else
 		psysh
 	fi
+}
+
+function laravel-new-sail {
+    # exit if there's no argument
+    if [ -z "$1" ]; then
+        echo "App name required"
+        return 1
+    fi
+    open-docker
+    curl -s "https://laravel.build/$1" | bash
 }
