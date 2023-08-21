60dad938d71f3844571cd40d61b11eb14baa2fdc
Jason Varga <jason@pixelfear.com>
2023-08-21 14:38:09 -0400

    i dont use these

--- zsh/bundles/statamic.zsh
+++ zsh/bundles/statamic.zsh
@@ -4,9 +4,6 @@ alias asc='plz stache:clear'
 alias pegs='p --exclude-group slow'
 alias cucms='comp update statamic/cms'
 alias cmspath='comp show statamic/cms --path'
-alias s1='cd ~/code/statamic/one'
-alias s2='cd ~/code/statamic/two'
-alias s3='cd ~/code/statamic/three'
 alias cms='cd ~/code/statamic/cms'
 alias plzuser="cp $DOTFILES/statamic/jason@statamic.com.yaml users/jason@statamic.com.yaml && echo 'User created.'"
 alias plzpro="sed -i \"\" \"s/'pro'\ =>\ false/'pro'\ =>\ true/\" config/statamic/editions.php && echo 'Pro enabled.'"
