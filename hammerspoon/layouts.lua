6d184a702acb1ce54d11ac49602a261c5532a0d5
Jason Varga <jason@pixelfear.com>
2024-06-24 10:40:47 -0400

    adjust layouts

--- hammerspoon/layouts.lua
+++ hammerspoon/layouts.lua
@@ -6,11 +6,12 @@ return {
       { '12,0 18x20', positions.twoThirds.right, '6,0 24x20', positions.halves.right }
     },
     apps = {
-        Safari = { cell = 1, open = true },
-        Chrome = { cell = 1 },
-        Code = { cell = 2, open = true },
+        Arc = { cell = 1, open = true },
+        Code = { cell = 2 },
+        PhpStorm = { cell = 2, open = true },
         Tower = { cell = 2 },
         Tinkerwell = { cell = 2 },
+        TablePlus = { cell = 2 }
     }
   },
   {
@@ -21,11 +22,12 @@ return {
     },
     apps = {
         Ray = { cell = 1, open = true },
-        Safari = { cell = 2, open = true },
-        Chrome = { cell = 2 },
-        Code = { cell = 2, open = true },
+        Arc = { cell = 2, open = true },
+        Code = { cell = 2 },
+        PhpStorm = { cell = 2, open = true },
         Tower = { cell = 2 },
         Tinkerwell = { cell = 2 },
+        TablePlus = { cell = 2 },
     }
   },
   {
@@ -36,12 +38,13 @@ return {
       { '15,0 15x20', '17,0 13x20', positions.twoThirds.right, positions.twoThirds.right, positions.twoThirds.right },
     },
     apps = {
-        Safari = { cell = 2, open = true },
+        Arc = { cell = 2, open = true },
         Ray = { cell = 1, open = true },
-        Chrome = { cell = 2 },
-        Code = { cell = 3, open = true },
+        Code = { cell = 3 },
+        PhpStorm = { cell = 3, open = true },
         Tower = { cell = 3 },
         Tinkerwell = { cell = 3 },
+        TablePlus = { cell = 3 },
     }
   }
 }
