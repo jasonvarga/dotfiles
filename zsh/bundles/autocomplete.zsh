53e1ff7ef6dc284e7026dc192f114776f0a93257
Jason Varga <jason@pixelfear.com>
2025-02-08 11:00:19 -0500

    autocomplete tweaks

--- zsh/bundles/autocomplete.zsh
+++ zsh/bundles/autocomplete.zsh
@@ -1,5 +1,10 @@
 zstyle ':autocomplete:*' widget-style menu-select
 zstyle ':autocomplete:*' fzf-completion yes
-zstyle ':autocomplete:*' list-lines 5
+zstyle ':autocomplete:*' list-lines 10
 zstyle ':autocomplete:*' min-delay 0.2
 zstyle ':autocomplete:*' min-input 1
+
+# Make left/right arrow keys move the cursor in the menu.
+# Without this, they would also go up/down the options.
+bindkey -M menuselect  '^[[D' .backward-char  '^[OD' .backward-char
+bindkey -M menuselect  '^[[C'  .forward-char  '^[OC'  .forward-char
