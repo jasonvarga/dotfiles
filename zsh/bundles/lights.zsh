143d598bba111241ccc3559fe251bd0cfd7df337
Jason Varga <jason@pixelfear.com>
2025-08-03 12:09:42 -0400

    backlight on and off

--- zsh/bundles/lights.zsh
+++ zsh/bundles/lights.zsh
@@ -20,6 +20,12 @@ backlight() {
         normal)
             backlight purple
             ;;
+        on)
+            backlight_on
+            ;;
+        off)
+            backlight_off
+            ;;
         *)
             echo "Unknown color: $color"
             ;;
@@ -51,6 +57,50 @@ backlight_rgb() {
                     \"g\": $g,
                     \"b\": $b
                 }
+            }
+        }
+    " > /dev/null
+}
+
+backlight_on() {
+    device="C0:30:D8:39:32:37:38:3D"
+    model=H6056
+
+    curl --silent \
+     --request PUT \
+     --url https://developer-api.govee.com/v1/devices/control \
+     --header "Govee-API-Key: $GOVEE_API_KEY" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data "
+        {
+            \"device\": \"$device\",
+            \"model\": \"$model\",
+            \"cmd\": {
+                \"name\": \"turn\",
+                \"value\": \"on\"
+            }
+        }
+    " > /dev/null
+}
+
+backlight_off() {
+    device="C0:30:D8:39:32:37:38:3D"
+    model=H6056
+
+    curl --silent \
+     --request PUT \
+     --url https://developer-api.govee.com/v1/devices/control \
+     --header "Govee-API-Key: $GOVEE_API_KEY" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data "
+        {
+            \"device\": \"$device\",
+            \"model\": \"$model\",
+            \"cmd\": {
+                \"name\": \"turn\",
+                \"value\": \"off\"
             }
         }
     " > /dev/null
